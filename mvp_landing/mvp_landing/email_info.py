30e7f49cd25a1b860cc19bc05c60ae6ba813fca6
dipenjethva19 <dipenjethva19@gmail.com>
2016-06-28 23:15:39 -0700

    Initial commit

---
+++ mvp_landing/mvp_landing/email_info.py
@@ -0,0 +1,6 @@
+#for gmail -- google apps
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "dipenjethva19@gmail.com"
+EMAIL_HOST_PASSWORD = "9427749711gmail"
+EMAIL_PORT = 587
